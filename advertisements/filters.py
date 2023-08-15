a61827d03d507a58d694c4902421ce34fee479ae
Nikita <reymoro3@gmail.com>
2023-08-15 16:55:00 +0300

    Initial commit

---
+++ advertisements/filters.py
@@ -0,0 +1,12 @@
+from django_filters import rest_framework as filters
+from django_filters import DateFromToRangeFilter, FilterSet, BaseInFilter
+from .models import Advertisement
+
+
+class AdvertisementFilter(FilterSet):
+    created_at = DateFromToRangeFilter()
+    status = BaseInFilter(field_name='status')
+
+    class Meta:
+        model: Advertisement
+        fields = ['created_at', 'status']
